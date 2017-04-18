e9c761709e11cc3cbb349acdea212a7fc725d31a
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-04-18 20:30:21 +0530

    Update CourseSelectionController with Course Data

--- app/Http/Controllers/CourseSelectionController.php
+++ app/Http/Controllers/CourseSelectionController.php
@@ -3,27 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ProgramMaster;
+use App\CourseMaster;
+use App\ProgramCourseTerm;
 
 class CourseSelectionController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
+		  $program_id = $request->session()->get('program_id');
+		  $term_id = $request->session()->get('term_id');
+
+		  $coreCourses = $this->getCoreCourses($program_id, $term_id);
+		  $electiveCourses = $this->getElectiveCourses($program_id, $term_id);
+
+//		  return response()->json([
+//				  'coreCourses' => $coreCourses,
+//				  'electiveCourses' => $electiveCourses
+//		  ]);
+
     return view('course-selection')->with([
-      'core'=> [
-        'Computer Networks',
-        'Operating Systems',
-        'Design & Analysis of Algorithms',
-        'Design of Software Systems',
-        'Enterprice Computing'
-      ],
-      'electives'=> [
-        'Computer Organization'
-      ]
+      'coreCourses' => $coreCourses,
+      'electiveCourses' => $electiveCourses
     ]);
+
   }
 
   public function post(Request $request)
   {
     return redirect('/feedback');
   }
+
+
+		private function getCoreCourses($program_id, $term_id) {
+
+				$allCoreCourses = CourseMaster::where('course_type', 'Core')->get();
+				$coreCourses = array();
+
+				foreach ($allCoreCourses as $course) {
+						$term = ProgramCourseTerm::where('program_id', $program_id)
+								->where('term_id', $term_id)
+								->where('course_id', $course->course_id)
+								->first();
+						if (sizeof($term) > 0) array_push($coreCourses, $course);
+				}
+
+				return $coreCourses;
+
+		}
+
+		private function getElectiveCourses($program_id, $term_id) {
+
+				$allElectiveCourses = CourseMaster::where('course_type', 'Elective')->get();
+				$electiveCourses = array();
+
+				foreach ($allElectiveCourses as $course) {
+						$term = ProgramCourseTerm::where('program_id', $program_id)
+								->where('term_id', $term_id)
+								->where('course_id', $course->course_id)
+								->first();
+						if (sizeof($term) > 0) array_push($electiveCourses, $course);
+				}
+
+				return $electiveCourses;
+
+		}
 }
