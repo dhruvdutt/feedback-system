c9cfd5c1f2ca194f6d1d755e6cc412cdb05ad554
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-04-18 23:49:10 +0530

    Update QuestionSeeder

--- database/seeds/QuestionSeeder.php
+++ database/seeds/QuestionSeeder.php
@@ -31,11 +31,42 @@ class QuestionSeeder extends Seeder
           'lab' => true,
           'tutorial' => false
       ]);
+      // Custom Questions
       DB::table('question_master')->insert([
           'question_id' => '3',
           'course_id' => 'IT619',
           'term_id' => '1',
           'question' => 'How Was The Project?',
+          'answer_type_id' => '1',
+          'lecture' => true,
+          'lab' => true,
+          'tutorial' => false
+      ]);
+      DB::table('question_master')->insert([
+          'question_id' => '4',
+          'course_id' => 'IT612',
+          'term_id' => '3',
+          'question' => 'Custom Question 1',
+          'answer_type_id' => '1',
+          'lecture' => true,
+          'lab' => true,
+          'tutorial' => false
+      ]);
+      DB::table('question_master')->insert([
+          'question_id' => '5',
+          'course_id' => 'IT617',
+          'term_id' => '3',
+          'question' => 'Custom Question 2',
+          'answer_type_id' => '1',
+          'lecture' => true,
+          'lab' => true,
+          'tutorial' => false
+      ]);
+      DB::table('question_master')->insert([
+          'question_id' => '6',
+          'course_id' => 'IT616',
+          'term_id' => '3',
+          'question' => 'Custom Question 3',
           'answer_type_id' => '1',
           'lecture' => true,
           'lab' => true,
