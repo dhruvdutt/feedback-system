0f3703a5925b9c98d7a909355819c1c0dca8af2a
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-05-01 17:30:33 +0530

    Add excel-sheet import plugin

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+		      // maatwebsite/excelProvider: Excel Sheet Import Plugin
+		      'Maatwebsite\Excel\ExcelServiceProvider',
+
     ],
 
     /*
@@ -225,6 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+		      // maatwebsite/excel Plugin Alias
+				    'Excel' => 'Maatwebsite\Excel\Facades\Excel',
 
     ],
 
